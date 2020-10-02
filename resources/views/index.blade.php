d1ddf4b92a0020ec5be4634a92477823ea35608a
yonathan <yonathan.hartoko@ipnetsolusindo.com>
2020-10-02 22:10:44 +0700

    Remove table to div

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,53 @@
 <head>
 	<title>Portal Berita Mercu Buana</title>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	<style>
+		img{
+			width : 150px;
+		}
+	</style>
 </head>
 <body>
 
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="#">Logo</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+				<a class="nav-link" href="#">Home
+					<span class="sr-only">(current)</span>
+				</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#">About</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#">Services</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" href="#">Contact</a>
+				</li>
+			</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+	</div>
+
+	<br/>
+	<br/>
 	<div class="container">
 		<div class="card">
 			<div class="card-body">
-				
-
 				<h2 class="text-center"><a href="https://mercubuana.ac.id">PORTAL BERITA MERCU BUANA</a></h2>
 
 				<h3 class="text-center">Oleh</h3>
-                <h3 class="text-center">Kelompok 8</h3>
+				<h3 class="text-center">Kelompok 8</h3>
 
 				<p>Cari Berita :</p>
 
@@ -26,28 +61,36 @@
 					<input class="btn btn-primary ml-3" type="submit" value="CARI">
 				</form>
 
-                </br>
+				</br>
 
-                <p><a href="/addBerita">Klik disini untuk refresh berita</a></p>
+				<p><a href="/addBerita">Klik disini untuk refresh berita</a></p>
 
-				<table class="table table-bordered">
-					@foreach($feed as $p)
-					<tr>
-						<td><a href="{{ $p->link }}">{{ $p->title }}</a></td>
-						<td>{{ $p->date }}</td>
-						<td>{{ $p->description }}</td>
-						<td>
-							<a class="btn btn-danger btn-sm" href="/hapusBerita/{{ $p->id }}">Hapus</a>
-						</td>
-					</tr>
-					@endforeach
-				</table>
+				<div class="row">
+					<div class="col-md-12 ">
+						<div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+							<div class="row">
+								@foreach($feed as $p)
+								<div class="col-md-6">
+									<div class="card h-100">
+										<div class="card-body">
+											<h4 class="card-title">{{ $p->title }}</h4>
+											<p>{{ $p->date }}</p>
+											<p><?php echo $p->description; ?></p>
+										</div>
+										<div class="card-footer">
+											<a class="btn btn-danger btn-sm" href="{{ $p->link }}">Baca lebih lanjut</a>
+										</div>
+									</div>
+								</div>
+								@endforeach
+							</div>
+						</div>
+					</div>
+				</div>
 
 				{{ $feed ?? ''->links() }}
 			</div>
 		</div>
 	</div>
-
-
 </body>
 </html>
